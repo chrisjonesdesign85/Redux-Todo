f0a8d8df4aef124ce929636f04a1072eae0fa570
Chris Jones <chrisjonesdesign85@gmail.com>
2019-06-24 20:27:58 -0400

    made action creators

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TodoItem from './TodoItem';
 
 export default (props) => {
-    const todos = props.todo.map((todo, i) => <TodoItem todo={todo} key={i} index={i} />);
+    const todos = props.todos.map((todo, i) => <TodoItem todo={todo} key={i} index={i} />);
     return (
         <ul>
             {todos}
